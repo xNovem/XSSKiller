09e77c7ec0764f2915ac41ce370088c3d7886ba1
Novem <101331453+xNovem@users.noreply.github.com>
2023-07-31 17:38:56 +0300

    Update xsskiller.py
    fix

--- xsskiller.py
+++ xsskiller.py
@@ -1,28 +1,33 @@
 import os
 import requests
+from bs4 import BeautifulSoup
 
 def display_banner():
     banner_text = """
-  ____  _              _  __     __       
- / ___|| |_   _  ___  | | \ \   / /__  ___
-| |  _ | | | | |/ _ \ | |  \ \ / / _ \/ __|
-| |_| || | |_| |  __/ | |___\ V /  __/\__ \\
- \____||_|\__,_|\___| |______\_/ \___||___/
-                     XSS Killer
+__  ______ ____    _  ___ _ _
+\ \/ / ___/ ___|  | |/ (_) | | ___ _ __
+ \  /\___ \___ \  | ' /| | | |/ _ \ '__|
+ /  \ ___) |__) | | . \| | | |  __/ |
+/_/\_\____/____/  |_|\_\_|_|_|\___|_|
+                                   
     """
     version = "\033[1;31mV1.0\033[0m"
     print("\033[1;31m" + banner_text + "\033[0m" + " " + version)
 
 def check_xss_vulnerability(site_url):
     response = requests.get(site_url)
-    if "<script>" in response.text:
+    soup = BeautifulSoup(response.text, "html.parser")
+    script_tags = soup.find_all("script")
+    if script_tags:
         print("\033[1;32mXSS zafiyeti tespit edildi!\033[0m")
+        for i, tag in enumerate(script_tags, start=1):
+            print(f"{i}. <script> etiketi: {tag}")
     else:
         print("\033[1;31mXSS zafiyeti tespit edilemedi.\033[0m")
 
 def main():
     display_banner()
-    site_url = raw_input("Lutfen kontrol etmek istediginiz siteyi girin: ")
+    site_url = input("Lutfen kontrol etmek istediginiz siteyi girin: ")
     check_xss_vulnerability(site_url)
 
 if __name__ == "__main__":
